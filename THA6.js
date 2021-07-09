5ee7948c4f756e700ce870fa4182befc149304ca
shivani209 <62274662+shivani209@users.noreply.github.com>
2021-07-09 15:36:00 +0530

    Add files via upload

---
+++ THA6.js
@@ -0,0 +1,22 @@
+
+
+let obj1 = {
+    name: "Satya",
+    age:22,
+    output: function (...y){
+        let sum =0
+         y.forEach((e)=> sum += e) 
+        console.log(`Name = ${this.name} \n Age = ${this.age}\n Sum = ${sum} \ntype of input is array ? :  ${Array.isArray(y)}\n`)
+    }
+}
+
+let obj2 = {
+    name: "Prakash",
+    age: 30
+}
+
+obj1.output(obj2)
+obj1.output.call(obj2,1,2,3,4,5)
+obj1.output.apply(obj2,[11,52,31,4,15])
+let latefun = obj1.output.bind(obj2,11,52,31,4,15)
+latefun()
